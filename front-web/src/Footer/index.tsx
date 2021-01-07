7f2cb7d692c41f428b0b81d493e29e51034de2b8
lucas <lucas-mira1@hotmail.com>
2021-01-07 09:59:29 -0300

    Add footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css';
+import { ReactComponent as ReactComponent} from "./linkedin.svg";
+
+function Footer(){
+    return(
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior <br/> Lucas Mira 
+            <div className="footer-icons">
+                <a href="https://www.linkedin.com/in/lucas-mira-17514b203/" target="_new">
+                <ReactComponent/>
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
