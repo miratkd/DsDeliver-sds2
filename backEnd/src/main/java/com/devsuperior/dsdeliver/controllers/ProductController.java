39c49e40d7c8d8fc57583c891fd5850952eb035f
lucas <lucas-mira1@hotmail.com>
2021-01-10 21:40:31 -0300

    new prod version

--- backEnd/src/main/java/com/devsuperior/dsdeliver/controllers/ProductController.java
+++ backEnd/src/main/java/com/devsuperior/dsdeliver/controllers/ProductController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +27,13 @@ public class ProductController {
 		List<ProductDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity <ProductDTO> changeName(@PathVariable Long id, @RequestBody String text){
+		ProductDTO dto = service.changeName(id,text);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	
 
 }
--- backEnd/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
+++ backEnd/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -23,4 +23,14 @@ public class ProductService {
 		List<Product> list = repository.findAllByOrderByNameAsc();
 		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
 	}
+	
+	@Transactional
+	public ProductDTO changeName(Long id, String text){
+		Product product = repository.getOne(id);
+		String string[] = text.split("-");
+		product.setName(string[0]);
+		product.setDescription(string[1]);
+		product = repository.save(product);
+		return new ProductDTO(product);
+	}
 }
